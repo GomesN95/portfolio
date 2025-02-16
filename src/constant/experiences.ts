9283e3fed89cdf3c2ea6742cc55eb5c0ab6659c0
Gomes_N <gomes.nicolas95@gmail.com>
2025-02-16 22:14:04 +0100

    Experiences tard

--- src/constant/experiences.ts
+++ src/constant/experiences.ts
@@ -3,13 +3,20 @@ import { IExperience } from "@/interfaces/experience.interface";
 export const EXPERIENCES: IExperience[] = [
   {
     companyName: 'YSO Corp',
-    role: 'Developpeur Full stack / Responsable projet',
-    description: `Expérience riche et polyvalente en conception et développement de solutions pour des clients de renom
-      tels que BIC et Voodoo, ainsi que pour des projets internes.
-      Spécialisé en JavaScript/TypeScript et DevOps, j'ai piloté de nombreux projets de bout en bout,
-      de la définition des besoins à la mise en production, en assurant un suivi client complet.
-      Ce poste, rempli de défis et d'innovation, m'a permis de collaborer avec des acteurs du monde entier
-      et d'intervenir également sur les aspects marketing et commerciaux des solutions proposées.`,
+    // role: 'Developpeur Full stack / Responsable projet',
+    // description: `Expérience riche et polyvalente en conception et développement de solutions pour des clients de renom
+    //   tels que BIC et Voodoo, ainsi que pour des projets internes.
+    //   Spécialisé en JavaScript/TypeScript et DevOps, j'ai piloté de nombreux projets de bout en bout,
+    //   de la définition des besoins à la mise en production, en assurant un suivi client complet.
+    //   Ce poste, rempli de défis et d'innovation, m'a permis de collaborer avec des acteurs du monde entier
+    //   et d'intervenir également sur les aspects marketing et commerciaux des solutions proposées.`,
+    role: 'Full Stack Developer / Project Manager',
+    description: `Rich and versatile experience in designing and developing solutions for renowned clients 
+      such as BIC and Voodoo, as well as internal projects. 
+      Specialized in JavaScript/TypeScript and DevOps, I have led numerous projects from start to finish, 
+      from requirements definition to production deployment, ensuring comprehensive client support. 
+      This position, filled with challenges and innovation, allowed me to collaborate with stakeholders worldwide 
+      and also contribute to the marketing and commercial aspects of the proposed solutions.`,
     skills: [
       'JavaScript/TypeScript',
       'Angular',
@@ -56,37 +63,57 @@ export const EXPERIENCES: IExperience[] = [
   },
   {
     companyName: 'ADES',
-    role: 'Developpeur Full stack C# / .NET - Alternance',
-    description: `Participation à la conception et au développement d'un Service Desk/CRM accessible sur desktop et web,
-      ainsi qu'une application mobile complémentaire.
-      Mon rôle a consisté à créer des fonctionnalités permettant une gestion efficace des interactions client
-      et un suivi des demandes en temps réel`,
+    // role: 'Developpeur Full stack C# / .NET - Alternance',
+    // description: `Participation à la conception et au développement d'un Service Desk/CRM accessible sur desktop et web,
+    //   ainsi qu'une application mobile complémentaire.
+    //   Mon rôle a consisté à créer des fonctionnalités permettant une gestion efficace des interactions client
+    //   et un suivi des demandes en temps réel`,
+    role: 'Full Stack Developer C# / .NET - Apprenticeship',
+    description: `Participated in the design and development of a Service Desk/CRM accessible on both desktop and web, 
+      as well as a complementary mobile application. 
+      My role involved creating features to enable efficient customer interaction management 
+      and real-time request tracking.`,
     skills: ['C#', '.NET', 'WPF', 'ASP.Net', 'Xamarin', 'SQLServer', 'Azure', 'Microsoft Graph', 'WINDev'],
     dates: ['2017', '2020'],
   },
   {
     companyName: 'YSO Corp',
-    role: 'Développeur AngularJs - Stage',
-    description: `Développement d'une API de test et création d'une application WordPress pour le site MyGSMData.
-      J'ai conçu et implémenté l'API pour automatiser et simplifier les tests de fonctionnalités,
-      améliorant ainsi l'efficacité des processus de validation`,
-    skills: ['AngularJs', 'Test unitaire', 'WordPress'],
+    // role: 'Développeur AngularJs - Stage',
+    // description: `Développement d'une API de test et création d'une application WordPress pour le site MyGSMData.
+    //   J'ai conçu et implémenté l'API pour automatiser et simplifier les tests de fonctionnalités,
+    //   améliorant ainsi l'efficacité des processus de validation`,
+    role: 'AngularJS Developer - Internship',
+    description: `Developed a test API and created a WordPress application for the MyGSMData website. 
+      I designed and implemented the API to automate and streamline functionality testing, 
+      thereby improving the efficiency of validation processes.`,
+    skills: ['AngularJs', 'Unit test', 'WordPress'],
     dates: ['2016', null],
   },
   {
-    companyName: 'Mairie de Villiers-Le-Bel',
-    role: 'Administrateur Réseaux - Stage',
-    description: `Stage en gestion de l'infrastructure informatique de la mairie,
-      incluant l'administration du serveur de données et la modernisation du parc informatique
-      sur une période de trois ans.
-      Sous la supervision de l'équipe IT, j'ai contribué à la maintenance quotidienne,
-      à la sécurisation des données et au suivi des projets de renouvellement du matériel pour
-      améliorer les performances des systèmes.`,
+    companyName: ' Villiers-Le-Bel Town hall',
+    // role: 'Administrateur Réseaux - Stage',
+    // description: `Stage en gestion de l'infrastructure informatique de la mairie,
+    //   incluant l'administration du serveur de données et la modernisation du parc informatique
+    //   sur une période de trois ans.
+    //   Sous la supervision de l'équipe IT, j'ai contribué à la maintenance quotidienne,
+    //   à la sécurisation des données et au suivi des projets de renouvellement du matériel pour
+    //   améliorer les performances des systèmes.`,
+    // skills: [
+    //   'Administration Serveurs / parc informatique', 'Windows OS',
+    //   'Active Directory', 'Administration Comptes Utilisateurs',
+    //   'Maintenance du reseau',
+    //   'Assistance / Support Utilisateurs', 'Incidents Matériels / Logiciels',
+    // ],
+    role: 'Network Administrator - Internship',
+    description: `Internship in managing the IT infrastructure of the town hall, 
+      including data server administration and modernizing the computer network over a three-year period. 
+      Under the supervision of the IT team, I contributed to daily maintenance, 
+      data security, and overseeing hardware renewal projects to improve system performance.`,
     skills: [
-      'Administration Serveurs / parc informatique', 'Windows OS',
-      'Active Directory', 'Administration Comptes Utilisateurs',
-      'Maintenance du reseau',
-      'Assistance / Support Utilisateurs', 'Incidents Matériels / Logiciels',
+      'Server / IT infrastructure administration', 'Windows OS',
+      'Active Directory', 'User Account Management',
+      'Network Maintenance',
+      'User Support / Assistance', 'Hardware / Software Incident Management',
     ],
     dates: ['2013', '2015'],
   },
