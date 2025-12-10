932aa9a490fa1a778ad76d29c915ad191c07ad3f
Gomes_N <gomes.nicolas95@gmail.com>
2025-12-10 21:55:06 +0100

    Update React Flight/Next.js RCE vulnerability

--- src/constant/project.ts
+++ src/constant/project.ts
@@ -2,11 +2,39 @@ import { IProject } from "@/interfaces/project.interface";
 
 export const PROJECTS: IProject[] = [
   {
+    name: 'Portfolio',
+    client: 'NG',
+  },
+  {
     name: 'Design My Bic',
     client: 'BIC',
   },
   {
     name: 'Voodoo Blog',
     client: 'Voodoo',
+  },
+  {
+    name: 'Monster Playable',
+    client: 'YSO Corp',
+  },
+  {
+    name: 'YSO Network',
+    client: 'YSO Corp',
+  },
+  {
+    name: 'GameUtils',
+    client: 'YSO Corp',
+  },
+  {
+    name: 'Sartliva',
+    client: 'YSO Corp',
+  },
+  {
+    name: 'Hit The Road',
+    client: 'LCE',
+  },
+  {
+    name: 'InovoiZ',
+    client: 'InovoiZ',
   },
 ];
